3927c8f778a510cab040b6c3fc7ab72543c7157c
Youying Lin <liny33@cs.washington.edu>
2016-05-23 12:04:42 -0700

    Adapt the size of leaflet layer to viz's layer with padding

--- map.js
+++ map.js
@@ -1,6 +1,30 @@
+var view; // Global variable
+d3.json("specs/us-airports.json", function(error, spec) {
+  vg.parse.spec(spec, function(error, chart) { 
+    view = chart({el:"#viz"}).update(); 
+    setTileSize();
+    parseMap();
+  });
+});
 
-// For leaflet
-var map = L.map('mapple').setView([-41.2858, 174.78682], 14);
+function setTileSize() {
+  // Get the paddings
+  var topPadding    = view.padding().top;
+  var leftPadding   = view.padding().left;
+  var rightPadding  = view.padding().right;
+  var bottomPadding = view.padding().bottom; 
+  var finalWidth    = leftPadding + rightPadding + view.width() + "px";
+  var finalHeight   = topPadding + bottomPadding + view.height() + "px";
+  
+  document.getElementById('mapple').style.width  = finalWidth;
+  document.getElementById('viz').style.width     = finalWidth;
+  document.getElementById('mapple').style.height = finalHeight;
+  document.getElementById('viz').style.height    = finalHeight;
+}
+
+function parseMap() {
+  // For leaflet
+  var map = L.map('mapple').setView([-41.2858, 174.78682], 14);
       mapLink = 
           '<a href="http://openstreetmap.org">OpenStreetMap</a>';
       L.tileLayer(
@@ -8,12 +32,4 @@ var map = L.map('mapple').setView([-41.2858, 174.78682], 14);
           attribution: '&copy; ' + mapLink + ' Contributors',
           maxZoom: 14,
           }).addTo(map);
-          
-// Vega
-var view; // Global variable
-d3.json("spec.json", function(error, spec) {
-  vg.parse.spec(spec, function(error, chart) { 
-    view = chart({el:"#viz"});
-    view.update(); 
-  });
-});
+}
