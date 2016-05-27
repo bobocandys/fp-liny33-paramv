0287f5f59268b7f7e3e41292bc625757042b7319
Youying Lin <liny33@cs.washington.edu>
2016-05-26 21:41:18 -0700

    Add comments

--- maps.js
+++ maps.js
@@ -2,11 +2,16 @@
 var view; 
 var map;
 
+/**
+ * Use d3 to parse a json spec into a Vega spec.
+ * Use Vega runtime to parse the Vega spec and generates an interactive
+ * visualization with leaflet underneath the visualization.
+ */
 d3.json("specs/us-airports-mercator.json", function(error, spec) {
   vg.parse.spec(spec, function(error, chart) { 
     view = chart({el:"#viz"}).update(); 
-    setTileSize();
-    parseMap();
+    setDivSize();
+    initializeMap();
     adaptViewToMap();
     map.on('move', function(e) {
       adaptViewToMap();
@@ -14,27 +19,27 @@ d3.json("specs/us-airports-mercator.json", function(error, spec) {
   });
 });
 
-function setTileSize() {
-  // Get the paddings
-  var topPadding    = view.padding().top;
-  var leftPadding   = view.padding().left;
-  var rightPadding  = view.padding().right;
-  var bottomPadding = view.padding().bottom; 
-  var finalWidth    = leftPadding + rightPadding + view.width() + "px";
-  var finalHeight   = topPadding + bottomPadding + view.height() + "px";
+/**
+ * Sets the size of the div contains leaflet to the size of the Vega visualization.
+ */
+function setDivSize() {
+  // Get the paddings to compute final width and height
+  var padding = view.padding();
+  var finalWidth    = padding.left + padding.right + view.width() + "px";
+  var finalHeight   = padding.top + padding.bottom + view.height() + "px";
   
   document.getElementById('mapple').style.width  = finalWidth;
-  document.getElementById('viz').style.width     = finalWidth;
   document.getElementById('mapple').style.height = finalHeight;
-  document.getElementById('viz').style.height    = finalHeight;
 }
 
- // For leaflet
-function parseMap() {
+/**
+ * Initialize the leaflet map to contain all points in the Vega visualization
+ * with the possible maximum zoom level.
+ */
+function initializeMap() {
   /*
-  {s} means one of the available subdomains
-  {z} — zoom level
-  {x} and {y} — tile coordinates
+  Default location is [47.653286, -122.305957]: 
+      Paul G. Allen Center for Computer Science & Engineering
   */
   map = L.map('mapple').setView([47.653286, -122.305957], 14);
   mapLink = 
@@ -45,7 +50,7 @@ function parseMap() {
       maxZoom: 14,
       }).addTo(map);
   
-  // Have leaflet zoom to fit all the data points
+  // Find the bounds in Vega visualization.
   var geoBounds = view.data('geoBounds').values()[0];  
   var minLat = geoBounds.minLat, 
     maxLat = geoBounds.maxLat, 
@@ -54,16 +59,20 @@ function parseMap() {
   var southWest = L.latLng(minLat, minLon),
     northEast = L.latLng(maxLat, maxLon),
     bounds = L.latLngBounds(southWest, northEast);
-
+    
+  // Sets the map view to fit the bounds.
   map.fitBounds(bounds);
 }
 
+/**
+ * Change the signal of the view so that the view is synced with the map.
+ */
 function adaptViewToMap() {
-  // Works fine without padding in the spec
+  var padding = view.padding();
   view.signal("geoCenterLat", map.getCenter()["lat"]);
   view.signal("geoCenterLon", map.getCenter()["lng"]);
   view.signal("geoScale", (1 << 8 + map.getZoom()) / 2 / Math.PI);
-  view.signal("geoTranslateX", map.getSize()["x"] / 2)
-  view.signal("geoTranslateY", map.getSize()["y"] / 2);
+  view.signal("geoTranslateX", map.getSize()["x"] / 2 - padding.left);
+  view.signal("geoTranslateY", map.getSize()["y"] / 2 - padding.top);
   view.update();
 }
